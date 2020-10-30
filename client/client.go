e5cc62fe59630fb83dd59feb61d1810327ffeb9c
terry0615 <terry_hsiesh@mail.chungyo.net>
2020-10-30 13:57:43 +0800

    Create client.go

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package main
+
+///hi test
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	//獲取服務端訊息
+	go ioCopy(os.Stdout, conn)
+	//將使用者輸入的文字訊息傳送到到服務端
+	ioCopy(conn, os.Stdin)
+}
+
+func ioCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
